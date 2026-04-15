6e27ffa2b57ea73b1d122a7eb6c4379178e21cd1
Purbanka <purbankaghosh1708@gmail.com>
2026-04-15 21:36:16 +0530

    updated one of the team member's name

--- src/components/Team/TeamData.js
+++ src/components/Team/TeamData.js
@@ -16,7 +16,7 @@ const TeamData = [
   { id: 9, name: "Tuli Mukherjee", role: "Academic Associate", img: "https://res.cloudinary.com/dttquxaro/image/upload/v1769064813/Tuli-pic_1_zsoinz.jpg", group: "operations" },
   { id: 10, name: "Zakir", role: "Facility Support Staff", img: "https://res.cloudinary.com/dttquxaro/image/upload/v1769064813/Zakir-pic_1_kyy6gz.jpg", group: "operations" },
   { id: 11, name: "Saunak Debnath", role: "Faction Olympiads Editor", img: "https://res.cloudinary.com/dttquxaro/image/upload/v1769065427/Saunak_drhmrg.jpg", group: "operations" },
-  { id: 12, name: "Tapasree Debnath", role: "Student support executive", img: "https://res.cloudinary.com/dttquxaro/image/upload/v1776268846/Tapashree_du7yc8.jpg", group: "operations" },
+  { id: 12, name: "Tapashree Debnath", role: "Student support executive", img: "https://res.cloudinary.com/dttquxaro/image/upload/v1776268846/Tapashree_du7yc8.jpg", group: "operations" },
 
   // Content / Web & Tech
   
